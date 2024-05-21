2e814f16f0d3b7e22421c158eef7ca88049c534f
Rakesh Kumar <rakeshvendrapati173@gmail.com>
2024-05-21 11:47:22 +0530

    Initial Commit

---
+++ MessagePublisher/src/main/java/com/uol/message/MessagePublisher/MessagePublisherApplication.java
@@ -0,0 +1,13 @@
+package com.uol.message.MessagePublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagePublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagePublisherApplication.class, args);
+	}
+
+}
---
+++ MessagePublisher/src/main/java/com/uol/message/MessagePublisher/producer/HelloMessage.java
@@ -0,0 +1,16 @@
+package com.uol.message.MessagePublisher.producer;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service // To Know The Spring that Our  Class is Service.
+public class HelloMessage {
+	@Autowired //Auto wired is Because the Spring Will Create the Object Of That Class To Us . 
+	private RabbitTemplate rabbitTemplate;
+	
+	// This MMethod is Declared to Know Send the Message
+	public void sendMessage(String name) {
+		rabbitTemplate.convertAndSend("Hello This Is My First Project imn Gradle My Name is  : "+ name);
+	}
+}
